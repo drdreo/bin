b2d5c94abd0ef8514c88add9c49242bd129bc104
Andreas Hahn <drdreo@users.noreply.github.com>
2018-05-22 11:33:42 +0200

    refactored colors

--- javascript.js
+++ javascript.js
@@ -85,7 +85,7 @@ function randomArrayItem(items){
   Inserts a GET parameter into the URL.
   Pass a key and its value.
 */
-function insertGETParam(key, value) {
+function addGETParam(key, value) {
     key = encodeURI(key);
     value = encodeURI(value);
 
@@ -110,6 +110,15 @@ function insertGETParam(key, value) {
     var currUrl = getPathFromUrl(document.URL);
     history.pushState('data to be passed', 'Page', currUrl + '?' + kvp.join('&'));
 
+}
+
+function findGETParameter(parameterName) {
+    let result = null, tmp = [];
+    location.search.substr(1).split("&").forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
 }
 
 /*
@@ -138,29 +147,13 @@ function removeGETParam(key) {
 
 /*
   Create a-tags from a passed URI
+  depends on URI library: https://medialize.github.io/URI.js/docs.html
 */
 function highlightURLs(source)
 {
     return URI.withinString(source, function(url) {
         return "<a href='" + url + "' target='_blank'>" + url + "</a>";
     });
-}
-
-//JS Output
-/*
-    Console log every key and its value from a passed object.
-*/
-function consoleIterateObject(data){
-  Object.keys(data).forEach(function(key){console.log(key+": ", data[key]);});
-}
-
-/*
-  Console log any amount of passed arguments
-*/
-function out() {
-    for (var i = 0; i < arguments.length; i++) {
-        console.log("i: " + i + " A: " + arguments[i] + "\n");
-    }
 }
 
 //JS Random
@@ -174,35 +167,37 @@ function randomNumberBetween(min, max){
 //JS Colors
 
 /*
-  Generates a random color and returns its hex and rgb value.
+  Generates a random color and returns its hex and rgb value as object.
+  
+  let color = generateRandomColor() // {hex: "#c1382f", rgb: "rgb(193,56,47)"}
 */
 function generateRandomColor() {
-        var r, g, b;
+        let r, g, b;
         r = ~~(Math.random() * 255);
         g = ~~(Math.random() * 255);
         b = ~~(Math.random() * 255);
 
         function componentToHex(c) {
-            var hex = c.toString(16);
+            let hex = c.toString(16);
             return hex.length === 1 ? "0" + hex : hex;
         }
 
-        var hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+        const hex = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
         return {hex: hex, rgb: "rgb(" + [r, g, b].join(',') + ")"};
 }
 
 /*
   Invert a rgb color string.
-  Extracts the values from a css sring: rgb(255,255,255) for example.
+  Extracts the values from a CSS RGB sring: rgb(255,255,255) and inverts it.
+  
+  let invertedRGBString = invertRGBColor("rgb(0,255,255)") // "rgb(255,0,0)"
 */
 function invertRGBColor(color) {
     color = color.substr(4, color.length - 5);
-    var c = color.split(",");
-
-
-    var r = 255 - c[0];
-    var g = 255 - c[1];
-    var b = 255 - c[2];
-
+    let c = color.split(",");
+    let r = 255 - c[0];
+    let g = 255 - c[1];
+    let b = 255 - c[2];
+  
     return "rgb(" + [r, g, b].join(',') + ")";
 }
